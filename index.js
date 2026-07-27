31a384871b51ce3626c731f2aa8bfa2270845cec
xiryuudev <farrel2008ganteng@gmail.com>
2026-07-27 21:28:53 +0700

    Part 2: Config & Message Handling

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import P from "pino";
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 (async () => {
+    const configModule = await import("./.config.json", { with: { type: "json" } });
+    const config = configModule.default;
     const {state, saveCreds} = await useMultiFileAuthState(".auth");
 
     const sock = makeWASocket({
@@ -30,10 +32,28 @@ const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
         }
     });
 
-    sock.ev.on("messages.upsert", async (m) => {
-        if (m.type !== "notify") return;
+    sock.ev.on("messages.upsert", async (messages) => {
+        if (messages.type !== "notify") return;
         
-        const msg = m.messages[0];
-        console.log(msg);
+        const msg = messages.messages[0];
+        const m = {
+            jid: msg.key.remoteJid || msg.key.remoteJidAlt,
+            fromMe: msg.key.fromMe,
+            number: msg.key.participantAlt || msg.key.participant || msg.key.remoteJid,
+            name: msg.pushName,
+            timestamp: msg.messageTimestamp,
+            message: msg.message,
+            text: msg.message?.conversation || msg.message?.extendedTextMessage?.text || msg.message?.imageMessage?.caption || msg.message?.videoMessage?.caption || msg.message?.documentMessage?.caption || ""
+        }
+        console.log(m);
+
+        switch (m.text) {
+            case 'hi':
+            case 'test':
+                await sock.sendMessage(m.jid, {text : `Hi... This is ${config.botName}, your personal assistant bot.`}, {quoted: msg})
+                break;
+            default:
+                break;
+        }
     })
 })()
